b7b0b4be513829f891aa70444779425f0f389186
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-04-29 20:14:50 +0530

    okau

--- frontend/src/component/LastYearAwards/LastYearAwardies.jsx
+++ frontend/src/component/LastYearAwards/LastYearAwardies.jsx
@@ -12,7 +12,7 @@ import SHitalWatile from "../../assets/ShitalWatile.jpg";
 import EknathKhedkar from "../../assets/EknathKhedkar.jpg";
 import RajveerBhardwaj from "../../assets/RajveerBhardwaj.jpg";
 import AtulkumarJain from "../../assets/Atul_kumar2.png";
-import MangeshKarad from "../../assets/MangeshTKarad.jpg";
+import MangeshKarad from "../../assets/MangeshTkarad.jpg";
 import AnitaDiwakar from "../../assets/AnitaDiwarkar2.jpg";
 import BharatAgarwal from "../../assets/BharatAgarwal.jpg";
 import RamakrishnanRaman from "../../assets/RamakrishnanRaman1.jpg";
